afb56a42df53f0422f9c2e49917bc320483cb202
kzk-maeda <casino.music0711@gmail.com>
2019-12-03 23:53:20 +0900

    com

---
+++ Swift5Bokete/Swift5Bokete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Bokete
+//
+//  Created by Kazuki Maeda on 2019/12/03.
+//  Copyright © 2019 Kazuki Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
