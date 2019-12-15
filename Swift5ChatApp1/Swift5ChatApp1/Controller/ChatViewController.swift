252695fec56df39e6fa3ee1d746bd056901344f9
kzk-maeda <casino.music0711@gmail.com>
2019-12-16 02:14:47 +0900

    Swift5ChatApp1

--- Swift5ChatApp1/Swift5ChatApp1/Controller/ChatViewController.swift
+++ Swift5ChatApp1/Swift5ChatApp1/Controller/ChatViewController.swift
@@ -15,6 +15,8 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet var tableView: UITableView!
     @IBOutlet var messageTextField: UITextField!
     
+    @IBOutlet var sendButton: UIButton!
+    
     // get screen size
     let screenSize = UIScreen.main.bounds.size
     
@@ -35,6 +37,10 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillHide(_ :)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
         // Fetch data from Firebase
+        fetchChatData()
+        
+        // delete cell line
+        tableView.separatorStyle = .none
         
     }
     
@@ -78,6 +84,8 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
         cell.messageLabel.text = chatArray[indexPath.row].message
+        cell.messageLabel.layer.cornerRadius = 20
+        cell.messageLabel.layer.masksToBounds = true
         cell.userNameLabel.text = chatArray[indexPath.row].sender
         cell.iconImageView.image = UIImage(named: "dogAvatarImage")
         
@@ -91,5 +99,57 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         return cell
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height / 10
+    }
+    
+    @IBAction func sendAction(_ sender: Any) {
+        messageTextField.endEditing(true)
+        messageTextField.isEnabled = false
+        sendButton.isEnabled = false
+        
+        // validation
+        if messageTextField.text!.count > 15 {
+            print("over 15 letters")
+            self.messageTextField.isEnabled = true
+            self.sendButton.isEnabled = true
+            return
+        }
+        
+        let chatDB = Database.database().reference().child("chats")
+        
+        // send key-value
+        let messageInfo = ["sender": Auth.auth().currentUser?.email, "message": messageTextField.text]
+        
+        chatDB.childByAutoId().setValue(messageInfo) { (error, result) in
+            if error != nil {
+                print(error as Any)
+            } else {
+                self.messageTextField.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextField.text = ""
+                print("send succeeded")
+            }
+        }
+        
+        // fetchChatData()
+    }
+    
+    func fetchChatData() {
+        let fetchDataRef = Database.database().reference().child("chats")
+        fetchDataRef.observe(.childAdded) { (snapshot) in
+            let snapshotData = snapshot.value as! AnyObject
+            let text = snapshotData.value(forKey: "message")
+            let sender = snapshotData.value(forKey: "sender")
+            
+            let message = Message()
+            message.message = text as! String
+            message.sender = sender as! String
+            self.chatArray.append(message)
+            self.tableView.reloadData()
+        }
+        
+    }
+    
 }
